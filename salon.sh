b2c95df12ce19d47161cd8c116a444215be58e28
alltaken21 <154517648+alltaken21@users.noreply.github.com>
2024-04-24 17:33:51 -0400

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,75 +1,55 @@
 #! /bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+SYMBOL=$1
 
-PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
-
-echo -e "\n ~~~~~ MY SALON ~~~~~\n"
-echo -e "Welcome to My Salon, how can I help you?\n"
-
-CUSTOMER_NAME=""
-CUSTOMER_PHONE=""
-SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
-
-CREATE_APPOINTMENT(){
-  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-  CUST_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-  CUST_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-  SERVICE_NAME_FORMATTED=$(echo $SERVICE_NAME | sed 's/\s//g' -E)
-  CUST_NAME_FORMATTED=$(echo $CUST_NAME | sed 's/\s//g' -E)
-  echo -e "\nWhat time would you like your $SERVICE_NAME_FORMATTED, $CUST_NAME_FORMATTED?"
-  read SERVICE_TIME
-  INSERTED=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUST_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-  echo -e "\nI have put you down for a $SERVICE_NAME_FORMATTED at $SERVICE_TIME, $CUST_NAME_FORMATTED."
-}
-
-MAIN_MENU(){
-echo -e "\nWhat's your phone number?"
-read CUSTOMER_PHONE
-
-HAVE_CUST=$($PSQL "SELECT customer_id, name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-
-if [[ -z $HAVE_CUST ]]
+if [[ -z $1 ]]
 then
-  echo -e "\nI don't have a record for that phone number, what's your name?"
-  read CUSTOMER_NAME
+  echo "Please provide an element as an argument."
+else
 
-  INSERTED=$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
-
-  CREATE_APPOINTMENT
-  else
-
-  CREATE_APPOINTMENT
-fi
-}
-
-
-
-LIST_SERVICES(){
-  if [[ $1 ]]
+  if [[ ! $SYMBOL =~ ^[0-9]+$ ]]
   then
-    echo -e "\n$1"
-  fi
 
-  echo "$SERVICES" | while read SERVICE_ID BAR NAME
-  do
-    echo "$SERVICE_ID) $NAME"
-  done
-
-  read SERVICE_ID_SELECTED
-  # if input is not a number
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
-  then
-    LIST_SERVICES "I could not find that service. What would you like today?"
-  else
-    HAVE_SERVICE=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-
-    if [[ -z $HAVE_SERVICE ]]
+    LENGTH=$(echo -n "$SYMBOL" | wc -m)
+    if [[ $LENGTH -gt 2 ]]
     then
-      LIST_SERVICES "I could not find that service. What would you like today?"
+
+      DATA=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING (type_id) WHERE name='$SYMBOL'")
+      if [[ -z $DATA ]]
+      then
+        echo "I could not find that element in the database."
+      else
+        echo "$DATA" | while read BAR BAR NUMBER BAR SYMBOL BAR NAME BAR WEIGHT BAR MELTING BAR BOILING BAR TYPE
+        do
+          echo "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $WEIGHT amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+        done
+      fi
     else
-      MAIN_MENU
+
+      DATA=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING (type_id) WHERE symbol='$SYMBOL'")
+      if [[ -z $DATA ]]
+      then
+        echo "I could not find that element in the database."
+      else
+        echo "$DATA" | while read BAR BAR NUMBER BAR SYMBOL BAR NAME BAR WEIGHT BAR MELTING BAR BOILING BAR TYPE
+        do
+          echo "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $WEIGHT amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+        done
+      fi
+    fi
+
+  else
+
+    DATA=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number=$SYMBOL")
+    if [[ -z $DATA ]]
+    then
+      echo "I could not find that element in the database."
+    else
+      echo "$DATA" | while read BAR BAR NUMBER BAR SYMBOL BAR NAME BAR WEIGHT BAR MELTING BAR BOILING BAR TYPE
+      do
+        echo "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $WEIGHT amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+      done
     fi
   fi
 
-}
-
-LIST_SERVICES
+fi
